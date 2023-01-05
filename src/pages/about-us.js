436f516bfc9424ddbf81887b59706532770ab6c6
s128804 <jafar.sabaouni@student.ap.be>
2023-01-05 22:28:00 +0100

    last commit

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -29,11 +29,7 @@ const AboutUs  = ({
   </Layout>)
 }
 
-/**
- * Head export to define metadata for the page
- *
- * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
- */
+
 export const Head = () => <Seo title="about-us" />
 
 
